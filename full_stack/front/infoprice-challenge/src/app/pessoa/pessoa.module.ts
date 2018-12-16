ce31398a5432f351c894c11539285dec8ab5b4c7
Everson <everson___@hotmail.com>
2018-12-16 11:05:02 -0200

    layout da lista

--- full_stack/front/infoprice-challenge/src/app/pessoa/pessoa.module.ts
+++ full_stack/front/infoprice-challenge/src/app/pessoa/pessoa.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../shared/material-module';
 import { PessoaService } from '../service/pessoa.service';
 import { PessoaListComponent } from './pessoa-list/pessoa-list.component';
 
 @NgModule({
   declarations: [PessoaListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   providers: [
     PessoaService
